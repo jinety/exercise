6435792f119389a49e50e5be9865a4aeb782b53f
Jine Ty <vhoa1000@gmail.com>
2022-03-25 16:01:51 +0700

    find parity

---
+++ thuctap.sh
@@ -0,0 +1,12 @@
+# FIND EVEN NUMBER OR ODD NUMBER IN SHELL SCRIPT
+clear 
+echo "---- EVEN OR ODD IN SHELL SCRIPT -----"
+echo -n "Enter a number:"
+read n
+echo -n "RESULT: "
+if [ `expr $n % 2` == 0 ]
+then
+	echo "$n It is even number"
+else
+	echo "$n It is odd number"
+fi
